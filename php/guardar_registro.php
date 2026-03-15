4b502badf80eabb05b8ceb5bdbd32ccb7f263a67
Frank <blender.webservices@gmail.com>
2026-03-15 08:33:50 -0600

    debug: Temporarily enable PHP error reporting for HTTP 500 debugging.

--- php/guardar_registro.php
+++ php/guardar_registro.php
@@ -1,4 +1,9 @@
 <?php
+// TEMPORAL: Habilitar reporte de errores para depurar el HTTP 500
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: text/html; charset=UTF-8');
 date_default_timezone_set('America/Mexico_City');
 
